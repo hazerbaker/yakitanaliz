c3293ecd3f5e2d6319dbf3dfc4bcdb7a821ace38
Çağdaş Hazer Bekar <cagdasinmaili@gmail.com>
2018-07-10 09:54:53 +0300

    login v1

--- ionic/src/pages/login/login.ts
+++ ionic/src/pages/login/login.ts
@@ -14,9 +14,9 @@ export class LoginPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
+  account: { username: string, password: string } = {
+    username: 'admin',
+    password: 'admin'
   };
 
   // Our translated text strings
@@ -37,7 +37,6 @@ export class LoginPage {
     this.user.login(this.account).subscribe((resp) => {
       this.navCtrl.push(MainPage);
     }, (err) => {
-      this.navCtrl.push(MainPage);
       // Unable to log in
       let toast = this.toastCtrl.create({
         message: this.loginErrorString,
